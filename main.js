2d7b125664412e39ce5605c05b999b6688dfbbdc
LunaABath <lunaagustinabath@gmail.com>
2024-01-18 19:30:56 -0300

    fix functions

--- main.js
+++ main.js
@@ -11,34 +11,6 @@ let page = 1;
 let itemsPerPage = 20;
 let offset = 0;
 
-/* change mode */
-const toggleDarkMode = () => {
-    $("#page-body").classList.toggle("bg-black");
-    $("#page-body").classList.toggle("bg-white");
-    $("#page-body").classList.toggle("text-white");
-    $("#page-body").classList.toggle("text-black");
-
-    $("#sun-light").classList.toggle("hidden");
-    $("#moon-dark").classList.toggle("hidden");
-    $("#results-container").classList.toggle("text-white");
-    $("#results-container").classList.toggle("text-black");
-};
-$("#sun-light").addEventListener("click", () => {
-    toggleDarkMode();
-    $("#sun-light").classList.add("hidden");
-    $("#moon-dark").classList.remove("hidden");
-});
-
-$("#moon-dark").addEventListener("click", () => {
-    toggleDarkMode();
-    $("#moon-dark").classList.add("hidden");
-    $("#sun-light").classList.remove("hidden");
-});
-
-
-
-
-
 
 /* llamado a la Api */
 const getMarvel = async (recurso, offset, itemsPerPage) => {
@@ -51,25 +23,24 @@ const getMarvel = async (recurso, offset, itemsPerPage) => {
 
 
 const printDataMarvel = (recurso, data) => {
-
     $("#count-results").innerHTML = `Resultados : ${data.length}`;
     $("#results").innerHTML = ``;
-
+  
     for (const item of data) {
-        let thumbnail = item.thumbnail.path + "." + item.thumbnail.extension;  /* NO FUNCIONAN LOS FOCUS*/
-        $("#results").innerHTML += `
+      let thumbnail = item.thumbnail.path + "." + item.thumbnail.extension; /* NO FUNCIONAN LOS FOCUS*/
+      $("#results").innerHTML += `
         <div tabindex="0" class="flex flex-col font-semibold w-56 h-100 m-2 p-2">
-            <div class="h-2/3">                                        
-                <img class="shadow-lg shadow-red-600/50 h-full w-full focus:translate-y-[5]" src="${thumbnail}" alt="img-${recurso}">
+            <div class="h-2/3  :focus:translate-y-[5]">                                        
+                <img class="shadow-lg shadow-red-600/50 h-full w-full" src="${thumbnail}" alt="img-${recurso}">
             </div>
             <div class="text-center mt-6 ">
                 <h1 class="text-sm :focus:text-red-600">${recurso === 'characters' ? item.name : item.title}</h1>
             </div>
         </div>
-        `
-        //console.log(item);
+        `;
+      //console.log(item);
     }
-};
+  };
 
 /* PAGINATION */
 
@@ -88,24 +59,33 @@ $("#search-btn").addEventListener("click", async () => {
     try {
         const response = await getMarvel(type, offset, itemsPerPage);
         marvelData = response.data;
+
         const filteredData = marvelData.results.filter(item =>
-            item.name.toLowerCase().includes(searchInput.toLowerCase()) ||
-            item.title.toLowerCase().includes(searchInput.toLowerCase())
+            (item.name || '').toLowerCase().includes(searchInput.toLowerCase()) || (item.title || '').toLowerCase.includes(searchInput.toLowerCase())
         );
 
         switch (sort) {
-            // case "newer":
-            //     filteredData.sort((a, b) => new Date(b.dates.date) - new Date(a.dates.date));
-            //     break;
-            // case "older":
-            //     filteredData.sort((a, b) => new Date(a.dates.date) - new Date(b.dates.date));
-            //     break;
-            // case "a-to-z":
-            //     filteredData.sort((a, b) => a.title.localeCompare(b.title));
-            //     break;
-            // case "z-to-a":
-            //     filteredData.sort((a,) => b.title.localeCompare(a.title));
-            //     break;
+            case "newer":
+                filteredData.sort((a, b) => new Date(b.dates.date) - new Date(a.dates.date));
+                break;
+            case "older":
+                filteredData.sort((a, b) => new Date(a.dates.date) - new Date(b.dates.date));
+                break;
+            case "a-to-z":
+                filteredData.sort((a, b) => {
+                    const titleA = a.title || a.name || '';
+                    const titleB = b.title || b.name || '';
+                   return titleA.localeCompare(titleB)
+                });
+                
+                break;
+            case "z-to-a":
+                filteredData.sort((a, b) => {
+                    const titleA = a.title || a.name || '';
+                    const titleB = b.title || b.name || '';
+                   return titleB.localeCompare(titleA)
+                });
+                break;
         }
 
         printDataMarvel(type, filteredData);
@@ -163,6 +143,30 @@ $("#last-page").addEventListener("click", async () => {
         updatePageInfo();
     }
     console.log('soy last');
+});
+
+/* change mode */
+const toggleDarkMode = () => {
+    $("#page-body").classList.toggle("bg-black");
+    $("#page-body").classList.toggle("bg-white");
+    $("#page-body").classList.toggle("text-white");
+    $("#page-body").classList.toggle("text-black");
+
+    $("#sun-light").classList.toggle("hidden");
+    $("#moon-dark").classList.toggle("hidden");
+    $("#results-container").classList.toggle("text-white");
+    $("#results-container").classList.toggle("text-black");
+};
+$("#sun-light").addEventListener("click", () => {
+    toggleDarkMode();
+    $("#sun-light").classList.add("hidden");
+    $("#moon-dark").classList.remove("hidden");
+});
+
+$("#moon-dark").addEventListener("click", () => {
+    toggleDarkMode();
+    $("#moon-dark").classList.add("hidden");
+    $("#sun-light").classList.remove("hidden");
 });
 
 
